7aaa707daaf93097eef334bc4480bb5cd8c0ccb5
Danny Dean <sboy76646@gmail.com>
2026-08-20 21:13:30 +0100

    Hide sell navigation from public secondary sidebar

--- client/src/components/home/HeroBanner.jsx
+++ client/src/components/home/HeroBanner.jsx
@@ -51,15 +51,6 @@ function HeroBanner() {
                     </NavLink>
                   ))}
                 </div>
-
-                <div className="mx-3 my-6 border-t border-slate-200" />
-
-                <Link
-                  to="/sell"
-                  className="mx-1 flex items-center justify-center rounded-lg border border-orange-500/30 bg-orange-50 px-3 py-3 text-center text-xs font-bold text-orange-600 transition hover:bg-orange-500 hover:text-white"
-                >
-                  Sell on FlexHub
-                </Link>
               </nav>
             </div>
           </aside>
